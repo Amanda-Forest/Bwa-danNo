7906d10ab1dac3fa0bbd29324f72dee6b5c4678f
Amanda-Forest <forestal253@gmail.com>
2025-11-15 13:34:03 -0800

    All my files

--- app/src/main/java/com/example/bwadanno/MainActivity.kt
+++ app/src/main/java/com/example/bwadanno/MainActivity.kt
@@ -1,20 +1,39 @@
 package com.example.bwadanno
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Declare our variables
+        val flashcardQuestion = findViewById<TextView>(R.id.flashcard_question)
+        val flashcardAnswer = findViewById<TextView>(R.id.flashcard_answer)
+
+        // function that make the answer visible
+        flashcardQuestion.setOnClickListener {
+            flashcardAnswer.visibility = View.VISIBLE
+            flashcardQuestion.visibility = View.INVISIBLE
+
+
+        }
+
+        flashcardAnswer.setOnClickListener {
+            flashcardQuestion.visibility = View.VISIBLE
+            flashcardAnswer.visibility = View.INVISIBLE
+
+
         }
     }
+
 }
